33109d577732a79599621015f0072909b277631a
Meghna Hens <61961884+MeghnaHens@users.noreply.github.com>
2020-04-02 19:32:09 +0530

    Add files via upload

---
+++ src/Appointment.java
@@ -0,0 +1,10 @@
+import java.time.format.DateTimeFormatter;
+
+public class Appointment {
+	User user;
+	Test test;
+	DateTimeFormatter date;
+	boolean approved;
+	
+	
+}
---
+++ src/User.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class User {
+	String userId;
+	List<DiagnosticCenter> centerList;
+	String userName,userPassword,userRole,emailId;
+	long contactNumber;
+}
