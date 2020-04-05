3f44054c45acb682237c56b2282e657669fe4483
Meghna Hens <61961884+MeghnaHens@users.noreply.github.com>
2020-04-06 00:40:14 +0530

    Delete DiagnosticCenterNameException.java

--- src/com/cg/hcs/exception/DiagnosticCenterNameException.java
+++
@@ -1,7 +0,0 @@
-package com.cg.hcs.exception;
-
-public class DiagnosticCenterNameException extends Exception{
-	public DiagnosticCenterNameException() {
-		System.out.println("Diagnostic ce nter name must be in characters only..!");
-	}
-}
