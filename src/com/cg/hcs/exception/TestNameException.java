5608b2c907ca972712ade036de4be107a5f12ae5
Meghna Hens <61961884+MeghnaHens@users.noreply.github.com>
2020-04-06 00:40:23 +0530

    Delete TestNameException.java

--- src/com/cg/hcs/exception/TestNameException.java
+++
@@ -1,7 +0,0 @@
-package com.cg.hcs.exception;
-
-public class TestNameException extends Exception{
-		public TestNameException() {
-			System.out.println("Test name must be in characters..!");
-		}
-}
